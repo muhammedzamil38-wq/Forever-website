ec57254b02241922c9f82407dd866097e4a49430
Muhammed Zamil <muhammedzamil38@gmail.com>
2025-12-23 16:37:48 +0400

    The product display is half done

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -4,7 +4,7 @@ import { products } from "../assets/assets";
 export const ShopContext = createContext()
 
 const ShopContextProvider = (props) => {
-    const currency = '';
+    const currency = '$';
     const delivery_fee = 10;
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false)
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
+
+const {productId} = useParams();
+const {products} = useContext(ShopContext)
+const [productData,setProductData] = useState(false);
+
+
+
+const fetchProductData = async () => {
+  products.map((item)=>{
+    if (item._id === productId) {
+      setProductData(item)
+      console.log(item)
+      return null ;
+    }
+  })
+}
+
+useEffect(()=>{
+  fetchProductData();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+},[ productId, products])
+
   return (
     <div>
       
