820dcfeafbe9403feadf60a57beb2594ed593ea2
Muhammed Zamil <muhammedzamil38@gmail.com>
2026-01-15 15:34:39 +0400

    csss added in genrating otp section

--- frontend/src/components/GenerateOTP.jsx
+++ frontend/src/components/GenerateOTP.jsx
@@ -34,9 +34,9 @@ const GenerateOTP = () => {
     <div className='max-w-100 m-auto p-5 bg-[#f9f9f9] rounded-sm shadow-lg '>
       <h2>Generate otp</h2>
       <form className='w-75 m-12.5 p-5 border border-[#ccc] rounded-sm' onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
-        <button type='submit'>Generate OTP</button>
+        <label className='font-bold text-2xl text-black'>Email</label>
+        <input className='w-full p-2.5 m-2.5 border border-[#ccc] rounded-sm' type="email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+        <button className='w-full p-2.5 bg-[#28a745] text-white border-0 rounded-sm cursor-pointer hover:bg-[#218838]'  type='submit'>Generate OTP</button>
       </form>
     </div>
   )
